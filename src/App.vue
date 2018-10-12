72f898c103f58c894b1c134833bc29d82bc92232
zhaoangus <zhao_angus@163.com>
2018-10-12 20:33:49 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="header">header</div>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      .tab-item
+        flex: 1
+        text-align: center
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div @click="handleClick">1</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Header',
+  data () {
+    return {
+      seller: {},
+      goods: {},
+      ratings: {}
+    }
+  },
+  methods: {
+    handleClick () {
+      axios.get('../../../static/data.json')
+        .then(function (res) {
+          console.log(res)
+        })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
