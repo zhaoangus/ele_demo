660e804b706f7193b35b6082e39bb08d2638239c
zhaoangus <zhao_angus@163.com>
2018-10-23 21:25:14 +0800

    rating 50%

--- src/components/food/Food.vue
+++ src/components/food/Food.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="move">
-    <div v-show="showFlag" class="food">
-      <div class="food-content" ref="food">
+    <div v-show="showFlag" class="food" ref="food">
+      <div class="food-content">
         <div class="image-header">
           <img :src="food.image">
           <div class="back" @click="hide">
@@ -29,6 +29,13 @@
           <h1 class="title">商品信息</h1>
           <p class="text">{{food.info}}</p>
         </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :select-type="selectType"
+          :only-content="onlyContent" :desc="desc"
+          :ratings="food.ratings"></ratingselect>
+        </div>
       </div>
     </div>
   </transition>
@@ -39,6 +46,10 @@ import BScroll from 'better-scroll'
 import Vue from 'vue'
 import Cartcontrol from '../cartcontrol/Cartcontrol'
 import Split from '../split/Split'
+import Ratingselect from '../ratingselect/Ratingselect'
+const POSITIVE = 0
+const NEGATIVE = 1
+const ALL = 2
 export default {
   name: 'Food',
   props: {
@@ -46,16 +57,26 @@ export default {
   },
   components: {
     Cartcontrol,
-    Split
+    Split,
+    Ratingselect
   },
   data () {
     return {
-      showFlag: false
+      showFlag: false,
+      selectType: ALL,
+      onlyContent: true,
+      desc: {
+        all: '全部',
+        positive: '推荐',
+        negative: '吐槽'
+      }
     }
   },
   methods: {
     show () {
       this.showFlag = true
+      this.selectType = ALL
+      this.onlyContent = true
       this.$nextTick(() => {
         if (!this.scroll) {
           this.scroll = new BScroll(this.$refs.food, {
@@ -176,4 +197,11 @@ export default {
         padding: 0 8px
         font-size: 12px
         color: rgb(77, 85, 93)
+    .rating
+      padding-top: 18px
+      .title
+        line-height: 14px
+        margin-left: 18px
+        font-size: 14px
+        color: rgb(7, 17, 27)
 </style>
