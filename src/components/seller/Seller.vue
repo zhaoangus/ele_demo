a996d33bf8b10f2c463784b9392bf1ec11e5324a
zhaoangus <zhao_angus@163.com>
2018-10-26 22:27:58 +0800

    sellers 50%

--- src/components/seller/Seller.vue
+++ src/components/seller/Seller.vue
@@ -1,13 +1,232 @@
 <template>
-  <div>seller</div>
+  <div class="seller" ref="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="desc border-1px">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
+            </div>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="item in seller.supports" :key="item.index">
+            <span class="icon" :class="classMap[item.type]"></span>
+            <span class="text">{{item.description}}</span>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picList">
+            <li v-for="pic in seller.pics"
+            class="pic-item" :key="pic.index">
+              <img :src="pic" width="120" height="90">
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+import Star from '../star/Star'
+import Split from '../split/Split'
 export default {
-  name: 'Seller'
+  name: 'Seller',
+  props: {
+    seller: {}
+  },
+  components: {
+    Star,
+    Split
+  },
+  watch: {
+    'seller' () {
+      this.$nextTick(() => {
+        this._initPics()
+      })
+    }
+  },
+  methods: {
+    _initScroll () {
+      if (!this.scroll) {
+        this.scroll = new BScroll(this.$refs.seller, {
+          click: true
+        })
+      }
+    },
+    _initPics () {
+      if (this.seller.pics) {
+        let picWidth = 120
+        let margin = 6
+        let width = (picWidth + margin) * this.seller.pics.length - margin
+        this.$refs.picList.style.width = width + 'px'
+        this.$nextTick(() => {
+          if (!this.picScroll) {
+            this.picScroll = new BScroll(this.$refs.picWrapper, {
+              scrollX: true,
+              eventPassthrough: 'vertical'
+            })
+          } else {
+            this.picScroll.refresh()
+          }
+        })
+      }
+    }
+  },
+  created () {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
+  mounted () {
+    this._initScroll()
+    this._initPics()
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  @import '../../common/stylus/mixin.styl'
+  .seller
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      padding: 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+      .desc
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, .1))
+        font-size: 0
+        .star
+          display: inline-block
+          margin-bottom: 8px
+          vertical-align: top
+        .text
+          display: inline-block
+          margin-right: 12px
+          line-height: 18px
+          vertical-align: top
+          font-size: 10px
+          color: rgb(77, 85, 93)
+      .remark
+        display: flex
+        padding-top: 18px
+        .block
+          flex: 1
+          text-align: center
+          border-right: 1px solid rgba(7, 17, 27, .1)
+          &:last-child
+            border: none
+          h2
+            margin-bottom: 4px
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .content
+            line-height: 24px
+            font-size: 10px
+            color: rgb(7, 17, 27)
+            .stress
+              font-size: 24px
+    .bulletin
+      padding: 18px 18px 0 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+      .content-wrapper
+        padding: 0 12px 16px 12px
+        border-1px(rgba(7, 17, 27, .1))
+        .content
+          line-height: 24px
+          font-size: 12px
+          color: rgb(240, 20, 20)
+      .supports
+        .support-item
+          padding: 16px 12px
+          border-1px(rgba(7, 17, 27, .1))
+          font-size: 0
+          &:last-child
+            border-none()
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 16px
+          height: 16px
+          margin-right: 6px
+          background-size: 16px 16px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          line-height: 16px
+          font-size: 12px
+          color: rgb(7, 17, 27)
+    .pics
+      padding: 18px
+      .title
+        margin-bottom: 12px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+      .pic-wrapper
+        width: 100%
+        overflow: hidden
+        white-space: nowrap
+        .pic-list
+          font-size: 0
+          .pic-item
+            display: inline-block
+            margin-right: 6px
+            width: 120px
+            height: 90px
+            &:last-child
+              margin: 0
 </style>
